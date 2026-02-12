a02d0e6cbd5a394bc8fadd4878991f8dc04d398d
visurachan <visurachandula@gmail.com>
2026-02-12 18:26:44 +0000

    ADded cors file

---
+++ src/main/java/com/careers/backend/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.careers.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // all endpoints
+                        .allowedOrigins("https://careers-backend-5enq.onrender.com") // only your frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
